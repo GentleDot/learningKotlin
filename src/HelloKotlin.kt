7f9abb35b1404895ff98db6aeb3fe224656aafc0
gentledot <gentledot.wp@gmail.com>
2020-06-25 01:06:57 +0900

    #02 Kotlin의 패키지 (project, module, package, file의 관계)

--- src/HelloKotlin.kt
+++ src/HelloKotlin.kt
@@ -1,3 +1,15 @@
+import com.example.edu.Person
+
 fun main(){
-    println("Hello Cotlin!");
+    println("Hello Cotlin!")
+
+    val person = Person("gentledot", 20)
+
+    println(person)
+    println(person.age)
+    println(person.name)
+
+    val num: Int = 20
+
+    println("name = ${person.name}, num = $num")
 }
---
+++ src/com/example/edu/Person.kt
@@ -0,0 +1,7 @@
+package com.example.edu
+
+class Person(val name: String, val age: Int){
+    override fun toString(): String {
+        return "Person(name='$name', age=$age)"
+    }
+}
