40450b5bce0ff4f33f9779be6068a6b265223f0e
gentledot <gentledot.wp@gmail.com>
2020-07-17 18:24:01 +0900

    #10 파일의 입출력 - FileWriter, FileReader

--- src/chap10/FileWrite.kt
+++ src/chap10/FileWrite.kt
@@ -1,19 +1,42 @@
 package chap10
 
 import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
 import java.io.IOException
+import java.lang.Exception
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
 
 fun main() {
+    val outString: String = "안녕하세요!\tHello\r\nWorId!" // ①문자열의 구성
+    val path = "C:\\Temp\\testText.txt"
 //    useFileWriter()
-    writingFunction()
+//    writingFunction(path, outString)
+//    fileWriteAndAppend(path, outString)
+
+    /*val writer = FileWriter(path, true)
+    try {
+        writer.write(outString)
+    } catch (e : Exception){
+
+    } finally {
+        writer.close()
+    }*/
+
+    FileWriter(path, true).use { it.write(outString) }
+    FileReader(path).use { println(it.readText()) }
 }
 
-private fun writingFunction() {
-    val outString: String = "안녕하세요!\tHello\r\nWorId!" // ①문자열의 구성
-    val path = "C:\\Temp\\testfile.txt"
+private fun fileWriteAndAppend(path: String, outString: String) {
+
+    val file = File(path)
+    file.writeText(outString)
+    file.appendText("\nDo Great Work!") // 파일에 문자열 추가
+}
+
+private fun writingFunction(path: String, outString: String) {
 
     val file = File(path)
     /*val printwriter = PrintWriter(file)
@@ -22,6 +45,10 @@ private fun writingFunction() {
     file.printWriter().use {
         it.println(outString)
     }
+
+    // BufferedWriter 사용
+    file.bufferedWriter().use { it.write(outString) }
+
 }
 
 private fun useFileWriter() {
